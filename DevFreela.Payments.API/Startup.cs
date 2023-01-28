77fb6896afc0590b1455d59d6b01debf655760f2
Muriel Aurelio <murielaaurelio@gmail.com>
2023-01-28 17:02:25 -0300

    Implementado Mensageria com RabbitMQ para realizar o fluxo de pagamento

--- DevFreela.Payments.API/Startup.cs
+++ DevFreela.Payments.API/Startup.cs
@@ -1,3 +1,4 @@
+using DevFreela.Payments.API.Consumers;
 using DevFreela.Payments.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -21,6 +22,9 @@ namespace DevFreela.Payments.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IPaymentService, PaymentService>();
+
+            // Para que isto fique rodando em background se deve fazer estar configuração
+            services.AddHostedService<ProcessPaymentConsumer>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
